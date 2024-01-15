e0bda5918f13ddb038b2a0b1f93533169cb2875a
Seth <53190848+Xx-Seth-xX@users.noreply.github.com>
2024-01-15 14:12:00 +0100

    Added jumping instructions

--- src/main.zig
+++ src/main.zig
@@ -8,7 +8,8 @@ const stdin = blk: {
 const VirtualMachine = struct {
     const Self = @This();
     const size: comptime_int = 10;
-    const MemType = i32;
+    const MemType = u8;
+    const debug: bool = false;
 
     memory: [size]MemType,
     ptr: usize,
@@ -21,12 +22,13 @@ const VirtualMachine = struct {
     // - 	Decrement the byte at the data pointer by one.
     // . 	Output the byte at the data pointer.
     // , 	Accept one byte of input, storing its value in the byte at the data pointer.
-    // [ 	If the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
-    // ] 	If the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.[a]
-
     fn execute_program(self: *Self) !void {
         while (self.iptr < self.program.len) {
             const instr = self.program[self.iptr];
+            if (debug) {
+                std.log.info("Memory: {any}, ptr: {}, iptr: {}", .{ self.memory, self.ptr, self.iptr });
+                std.log.info("Instruction: {c}", .{instr});
+            }
             switch (instr) {
                 '+' => self.inc(),
                 '-' => self.dec(),
@@ -34,14 +36,17 @@ const VirtualMachine = struct {
                 '<' => try self.decp(),
                 '.' => try self.put(),
                 ',' => try self.get(),
+                '[' => try self.loopb(),
+                ']' => try self.loope(),
                 else => {
                     std.log.err("Encountered invalid char {} at index {}", .{ instr, self.iptr });
                     return error.InvalidInstruction;
                 },
             }
             self.iptr += 1;
-            // std.log.info("Instruction: {c}", .{instr});
-            // std.log.info("Memory: {any}, ptr: {}", .{ vm.memory, vm.ptr });
+        }
+        if (debug) {
+            std.log.info("Memory: {any}, ptr: {}", .{ self.memory, self.ptr });
         }
     }
     fn reset(self: *Self) void {
@@ -72,24 +77,85 @@ const VirtualMachine = struct {
     }
     fn put(self: Self) !void {
         const c = self.memory[self.ptr];
-        try stdout.print("{}\n", .{c});
+        try stdout.print("{c}", .{c});
+        if (debug) try stdout.print("\n");
     }
     fn get(self: *Self) !void {
         var buff: [1]u8 = undefined;
         if (try stdin.read(&buff) == 0) {
             buff[0] = 0;
         }
-        self.memory[self.ptr] = buff[0];
+        self.memory[self.ptr] = @as(u8, @intCast(buff[0]));
+    }
+
+    // [ 	If the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ] command.
+    fn loopb(self: *Self) !void {
+        if (self.memory[self.ptr] != 0) {
+            return;
+        } else if (self.iptr >= self.program.len - 1) {
+            return error.UnmatchedOpeningBracket;
+        }
+
+        // Tracks nesting
+        var counter: usize = 0;
+        self.iptr += 1;
+        while (self.iptr < self.program.len) : (self.iptr += 1) {
+            switch (self.program[self.iptr]) {
+                '[' => counter += 1,
+                ']' => {
+                    if (counter == 0) {
+                        return;
+                    } else {
+                        counter -= 1;
+                    }
+                },
+                else => {},
+            }
+        }
+        return error.UnmatchedOpeningBracket;
+    }
+
+    // ] 	If the byte at the data pointer is nonzero, then instead of moving the instruction pointer forward to the next command, jump it back to the command after the matching [ command.[a]
+    fn loope(self: *Self) !void {
+        if (self.memory[self.ptr] == 0) {
+            return;
+        } else if (self.iptr == 0) {
+            return error.UnmatchedClosingBracket;
+        }
+        // Tracks nesting
+        var counter: usize = 0;
+        while (self.iptr > 0) : (self.iptr -= 1) {
+            switch (self.program[self.iptr - 1]) {
+                ']' => counter += 1,
+                '[' => {
+                    if (counter == 0) {
+                        self.iptr -= 1;
+                        return;
+                    } else {
+                        counter -= 1;
+                    }
+                },
+                else => {},
+            }
+        }
+        return error.UnmatchedClosingBracket;
     }
 };
 
 pub fn main() !void {
-    const program = "+++.,.+.";
+    const setup = "+++.>++.";
+    const body = "[[>+>+<<-]>>[-<<+>>]<<<->]";
+    const program = setup ++ body;
+    // hello world program
+    // const program = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
     std.log.info("Executing program: \"{s}\"", .{program});
     var vm = VirtualMachine.init(program);
-    try vm.execute_program();
-    std.log.info("Final state:", .{});
-    std.log.info("{}", .{vm});
+    vm.execute_program() catch |err|
+        {
+        std.log.err("Error executing machine", .{});
+        std.log.err("Final state = {}", .{vm});
+        return err;
+    };
     try stdout.writeByte('\n');
     return;
 }
